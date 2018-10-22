799df377264c724b88e9e09bfd93399ddf478ce3
Anton <savchenko_a@icloud.com>
2018-10-22 11:11:13 +0300

    flask

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -1,17 +1,33 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
-@app.route("/get_classifier_result/<version>", methods=['GET', 'POST'])
+
+@app.route("/get_classifier_result/<int:version>", methods=['GET', 'POST'])
 def return_classifier_result(version):
-    #TODO прочитать из полученного запроса json-контент
-    #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
-    #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
+    # TODO прочитать из полученного запроса json-контент
+    # В случае, если version==1, то должен вернуться json с версией и
+    # полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
+
+    # В случае, если version==0, то должен вернуться json с версией и
+    # полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
+
+    # GET -- получение информации от сервера
+    # POST -- сервер принимает данные для хранения
+
+    r = request.get_json()  # вытягивает json-данные из запроса POST
+    if version == 1:
+        return json.dumps({"version": 1, "predict": r["predict"]})
+    return json.dumps({"version": 0, "predict": r["old_predict"]})
+
 
 @app.route("/")
 def hello():
-    #TODO должна возвращатьс инструкция по работе с сервером
-    return
+    # TODO должна возвращатьс инструкция по работе с сервером
+    instruction = 'POST data to http://127.0.0.1:5000/get_classifier_result/1 to store it with ' \
+                 'version:1' + '\n' + 'and to http://127.0.0.1:5000/get_classifier_result/0 to store it with version:0'
+    return instruction
+
 
 if __name__ == "__main__":
     app.run()
--- homeworks/03/flask_server_tests.py
+++ homeworks/03/flask_server_tests.py
@@ -1,4 +1,3 @@
-
 import requests
 import json
 
@@ -6,11 +5,14 @@ url = 'http://127.0.0.1:5000/get_classifier_result/'
 headers = {'Content-type': 'application/json',
            'Accept': 'text/plain',
            'Content-Encoding': 'utf-8'}
-predict_value_pool = ["ququ", "khdkjfhdkjfhdkjfhdf", "рпоарпоарпоарп", 12, 143., {"key":"value"}]
+
+print(requests.get('http://127.0.0.1:5000/').text)
+predict_value_pool = ["ququ", "khdkjfhdkjfhdkjfhdf", "рпоарпоарпоарп", 12, 143., {"key": "value"}]
 for i in predict_value_pool:
-    data = {"predict" : i}
-    answer = requests.post(url + "1", data=json.dumps(data), headers=headers)
-    response = answer.json()
+    data = {"predict": i}
+    # отправляет на хранение
+    answer = requests.post(url + "1", data=json.dumps(data), headers=headers)  # class Response
+    response = answer.json()  # JSON type
     assert answer.status_code == 200
     assert response['version'] == 1
     assert response['predict'] == i
@@ -21,4 +23,5 @@ for i in predict_value_pool:
     assert answer.status_code == 200
     assert response['version'] == 0
     assert response['predict'] == i
+
 print("Test ok")
